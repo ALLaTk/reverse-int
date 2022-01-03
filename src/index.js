06d273746193be41f9189bd3e301f80a8cc7c747
ALLaTk <regicter1810@mail.ru>
2022-01-04 00:13:04 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function revers (n) {
+  n = Math.abs(n) + '';
+  let x = Array.from(n);
+  let y = x.reverse();
+  return Number(y.join(''));
 }
